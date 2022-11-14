76927deb19ee16005685bb41572f6d67b0d6fd7d
Piyanut Raunkaew <piyanut.rk@gmail.com>
2022-11-14 22:52:51 +0700

    Add project files.

---
+++ CSV Helper 1/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Globalization
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports CsvHelper
+
+Module Module1
+
+    Sub Main()
+        WriteClassObj()
+        Console.ReadKey()
+    End Sub
+
+    Sub WriteClassObj()
+        Dim records As New List(Of Foo)({
+                                        New Foo() With {.Id = 1, .Name = "One"},
+                                        New Foo() With {.Id = 2, .Name = "One"},
+                                        New Foo() With {.Id = 3, .Name = "One"},
+                                        New Foo() With {.Id = 4, .Name = "One"}
+                                        })
+        records.WriteCSV("D:\\CSV\\fileVB2.csv")
+    End Sub
+
+    Public Class Foo
+        Public Property Id As Integer
+        Public Property Name As String
+    End Class
+
+End Module
+
+Module MyExtensions
+    <Extension>
+    Public Sub WriteCSV(Of T)(list As IList(Of T), filePath As String)
+        Using writer As New StreamWriter(filePath)
+            Using csv As New CsvWriter(writer, CultureInfo.InvariantCulture)
+                csv.WriteRecords(list)
+            End Using
+        End Using
+    End Sub
+End Module
---
+++ CSV Helper 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CSV_Helper_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteClassObj();
+            Console.ReadKey();
+        }
+
+        static void WriteClassObj()
+        {
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "one" },
+                new Foo { Id = 2, Name = "one" },
+                new Foo { Id = 3, Name = "one" },
+                new Foo { Id = 4, Name = "one" },
+            };
+            records.WriteCSV("D:\\CSV\\file.csv");
+        }
+    }
+
+    internal class Foo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static class MyExtensions
+    {
+        public static void WriteCSV<T>(this IList<T> list,string filePath) where T : class
+        {
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(list);
+            }
+        }
+    }
+}
